b8bed38832514b87525095afbc4b4d6a9d04de63
daniel <paundanielionut@gmail.com>
2014-10-26 17:12:41 +0200

    Primul commit!

---
+++ Test/src/MAIN.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Daniel
+ * Clasa de test pentru GIT
+ */
+public class MAIN {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Programul Test functioneaza! ");
+	}
+
+}
